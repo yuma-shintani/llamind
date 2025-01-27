b2c2283eb20ed01b640b330da0581919d6a1f6c4
Yuma Shintani <yuma.shintani@ntt.com>
2025-01-27 09:33:49 +0900

    add process external link

--- electron/main.ts
+++ electron/main.ts
@@ -1,12 +1,12 @@
-import { BrowserWindow, app, ipcMain } from 'electron'
+import { BrowserWindow, app, ipcMain, shell } from "electron";
 // import { createRequire } from 'node:module'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import "./setting-store";
-import { settingStore } from './setting-store';
+import { settingStore } from "./setting-store";
 
 // const require = createRequire(import.meta.url) // require is not defined
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // The built directory structure
 //
@@ -17,75 +17,88 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // │ │ ├── main.js
 // │ │ └── preload.mjs
 // │
-process.env.APP_ROOT = path.join(__dirname, '..')
+process.env.APP_ROOT = path.join(__dirname, "..");
 
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
-export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
+export const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
+export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
 
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
+  ? path.join(process.env.APP_ROOT, "public")
+  : RENDERER_DIST;
 
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'icon.png'),
-    titleBarStyle: 'hidden',
+    icon: path.join(process.env.VITE_PUBLIC, "icon.png"),
+    titleBarStyle: "hidden",
+    titleBarOverlay: {
+      color: "#FFF",
+      symbolColor: "#000",
+    },
     width: 1100,
     height: 760,
     center: true,
     trafficLightPosition: { x: 18, y: 18 },
     webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
+      preload: path.join(__dirname, "preload.mjs"),
     },
-  })
+  });
 
   // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
 
-  win.on('enter-full-screen', () => {
-    win?.webContents.send('enter-full-screen-message', []);
-  })
+  win.on("enter-full-screen", () => {
+    win?.webContents.send("enter-full-screen-message", []);
+  });
 
-  win.on('leave-full-screen', () => {
-    win?.webContents.send('leave-full-screen-message', []);
+  win.on("leave-full-screen", () => {
+    win?.webContents.send("leave-full-screen-message", []);
   });
 
   if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
+    win.loadURL(VITE_DEV_SERVER_URL);
   } else {
     // win.loadFile('dist/index.html')
-    win.loadFile(path.join(RENDERER_DIST, 'index.html'))
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
   }
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-ipcMain.handle('setting-set', async (_event, key, value) => {
+ipcMain.handle("setting-set", async (_event, key, value) => {
   await settingStore.set(key, value);
-})
+});
 
-ipcMain.handle('setting-get', async (_event, key) => {
+ipcMain.handle("setting-get", async (_event, key) => {
   return await settingStore.get(key);
-})
+});
 
-app.whenReady().then(createWindow)
+// IPC経由で外部リンクを開く処理
+ipcMain.handle("open-external-link", (_event, url: string) => {
+  if (url.startsWith("https://")) {
+    shell.openExternal(url);
+  }
+});
+
+app.whenReady().then(createWindow);
